a01417223242825696d6016e60b1d78ae2f37fc8
Apuolmajok <123954682+Apuolmajok@users.noreply.github.com>
2024-03-27 23:19:21 +0300

    configuring the deployment

--- QNova/QNova/QNova/urls.py
+++ QNova/QNova/QNova/urls.py
@@ -24,7 +24,13 @@ urlpatterns = [
     path('', include('home.urls')),
     path('cart/', include('cart.urls')),
     path('', include('quick.urls')),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+]
+   
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 
 
